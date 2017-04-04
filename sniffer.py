86382e26cbb1baac6799152a5f970b71d01e0b66
Ricardo Castro <jarvis@JARVIS.local>
2017-04-03 22:49:17 -0400

    Created packet sniffer.

--- sniffer.py
+++ sniffer.py
@@ -5,3 +5,17 @@ Author: Ricardo Castro Rodriguez CE
 Date: 3.4.17
 
 '''
+from scapy.all import *
+import os
+import sys
+
+def packet_sniffer():
+    print "Begin Sniffer"
+    pkt = scapy.all.sniffer(iface = "eth0", proto = "tcp")
+    pkt.show()
+
+
+if __name__ == '__main__':
+    def display():
+        version = 0.1
+        print "Packet Sniffer V{0}".format(version)
