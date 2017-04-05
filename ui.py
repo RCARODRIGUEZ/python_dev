d4884c496be2f19a41519e34970dddc164a48d8e
Ricardo Castro <jarvis@JARVIS.local>
2017-04-05 07:42:32 -0400

    Adding instructions to the user interface.

--- ui.py
+++ ui.py
@@ -2,11 +2,12 @@
 User Interface
 '''
 import argparse
-
-parser = argparse.ArgumentParser(description='Packet Sniffer')
-parser.add_argument('-ip','--address', help = "Enter ip address manually.",
-                    action = "store_true")
+version = 0.1
+parser = argparse.ArgumentParser(description='Packet Sniffer UI V{0}'.format(version))
+parser.add_argument('-ip','--address', help = "Enter ip address manually.", action = "store_true")
+parser.add_argument('-p', '--protocol', help = "Enter specific protocol.", action = "store_true")
 parser.add_argument('-v','--verbose', action = "count", help = "Display all the process.")
+parser.add_argument('-s','--sniffer', action = "count", help = "Apply sniffer")
 inst = parser.parse_args()
 
 if inst.verbose:
