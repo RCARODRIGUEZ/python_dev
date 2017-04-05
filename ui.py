c0ee5091c004c18837fc41f2446e4b173677881a
Ricardo Castro <jarvis@JARVIS.local>
2017-04-04 23:54:05 -0400

    Creating a UI for the program, but apart from it.

---
+++ ui.py
@@ -0,0 +1,15 @@
+'''
+User Interface
+'''
+import argparse
+
+parser = argparse.ArgumentParser(description='Packet Sniffer')
+parser.add_argument('-ip','--address', type = "string", help = "Enter ip address manually.",
+                    action = "store_true")
+parser.add_argument('-v','--verbose', action = "count", help = "Display all the process.")
+inst = parser.parse_args()
+
+if inst.verbose:
+    print "Entered Verbose"
+elif inst.address:
+    print "Entered Address"
